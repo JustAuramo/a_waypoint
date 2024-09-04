8873c09f6b3bfb038e6a64a2412f9421eeceebae
Auramo <108193124+JustAuramo@users.noreply.github.com>
2024-09-04 15:33:07 +0300

    Add files via upload

---
+++ client/waypoint.lua
@@ -0,0 +1,4 @@
+-- Muuttaa Waypointin värin
+CreateThread(function()
+    ReplaceHudColourWithRgba(142, 34, 139, 230, 255)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+version '1.0.0'
+author 'Auramo'
+description 'Muuttaa Waypointin väriä'
+
+
+client_scripts {
+    'client/*.lua'
+}
+
+
+
